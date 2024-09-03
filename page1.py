83b2ef8fe1f5255f19de5986734f46e802acd8e3
Jason Zheng <jzheng1995@gmail.com>
2024-09-02 22:10:16 -0700

    update app

--- page1.py
+++ page1.py
@@ -12,7 +12,7 @@ def main():
     st.write("# Canada Labour Characteristics")
 
 
-    st.write("This dashboard app is made from data extracted from the Monthly Labour Characteristics July 2024 survey by Statistics Canada (StatCan). More information on this dataset and study can be found on the [StatCan website](https://www150.statcan.gc.ca/n1/pub/71m0001x/71m0001x2021001-eng.htm).")
+    st.write("This dashboard app is made from data extracted from the Monthly Labour Characteristics July 2024 survey by Statistics Canada (StatCan). More information on this dataset and study can be found on the [StatCan website](https://www150.statcan.gc.ca/n1/pub/71m0001x/71m0001x2021001-eng.htm). The full app is hosted on [streamlit cloud](https://public-data-dashboard-zaexazhwocap2sucygqzre.streamlit.app/).")
 
     "## Number of study participants in July 2024"
     st_map = fmap(width = 1000)
